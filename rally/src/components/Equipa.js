1178fcef52de4660c13eb0870a4e9f3ba1504a4c
Diogo Correia <diogo.correia99@ua.pt>
2022-10-25 21:23:06 +0100

    /equipas/:id almost finished

--- rally/src/components/Equipa.js
+++ rally/src/components/Equipa.js
@@ -3,7 +3,12 @@ import './Equipa.css';
 import Navbar from "./Navbar";
 import { useParams } from "react-router-dom";
 
+import SHOT_GLASS from '../images/shot-glass.png';
+import PUKE from '../images/puke.png';
+
 function Equipa() {
+    const drinkPointsValue = 10;
+
     // get team id from url
     const { id } = useParams();
 
@@ -14,7 +19,7 @@ function Equipa() {
         document.querySelector('.team-name').innerText = data.name;
 
         // set initial points
-        document.querySelector(".points-value").innerText = data.members.length * 10;
+        document.querySelector(".points-value").innerText = data.members.length * drinkPointsValue;
 
         // members wrapper
         const members = document.querySelector('.members-list');
@@ -24,16 +29,26 @@ function Equipa() {
             const memberwrapper = document.createElement('div')
             members.appendChild(memberwrapper)
             memberwrapper.classList.add("memberwrapper")
+            // set attribute with member id
+            memberwrapper.setAttribute('data-member-id', member.id);
+
             const h1 = document.createElement('div')
             memberwrapper.appendChild(h1)
             h1.classList.add('membernames')
             h1.innerText=member.name
             
+            // drink image
+            const drink = document.createElement('img')
+            memberwrapper.appendChild(drink)
+            drink.src = SHOT_GLASS;
+            drink.title = "Bebidas";
+
             // dropdown with number of drinks
             const dropdown = document.createElement('select')
             memberwrapper.appendChild(dropdown)
             dropdown.classList.add('dropdown')
             dropdown.id = "drinks_"+member.id
+            dropdown.setAttribute('data-drinks', 1);
             for (let i = 0; i <= 10; i++) {
                 const option = document.createElement('option')
                 dropdown.appendChild(option)
@@ -41,6 +56,20 @@ function Equipa() {
                 option.innerText = i
             }
             dropdown.value = 1
+
+            // puke image
+            const puke = document.createElement('img')
+            memberwrapper.appendChild(puke)
+            puke.src = PUKE;
+            puke.title = "Vomitou?";
+
+            // checkbox to check if puked
+            const checkbox = document.createElement('input')
+            memberwrapper.appendChild(checkbox)
+            checkbox.type = "checkbox"
+            checkbox.id = "puked_"+member.id
+            checkbox.classList.add('checkbox-puked')
+            checkbox.value = false
         });
 
         // game wrapper
@@ -51,6 +80,7 @@ function Equipa() {
         game.appendChild(dropdown)
         dropdown.classList.add('dropdown')
         dropdown.id = "points"
+        dropdown.setAttribute('data-points', 0);
         for (let i = 0; i <= 100; i+=10) {
             const option = document.createElement('option')
             dropdown.appendChild(option)
@@ -58,6 +88,100 @@ function Equipa() {
             option.innerText = i
         }
         dropdown.value = 0
+
+        // egg wrapper
+        const egg = document.querySelector('.egg');
+
+        // checkbox to check if team has egg still
+        if (data.has_egg) {
+            document.querySelector('.egg > .section-title').style.marginBottom = "unset";
+            const checkbox = document.createElement('input')
+            egg.appendChild(checkbox)
+            checkbox.type = "checkbox"
+            checkbox.id = "egg"
+            checkbox.checked = data.has_egg
+        }
+        else {
+            // team lost egg
+            const h1 = document.createElement('div')
+            egg.appendChild(h1)
+            h1.classList.add('egglost')
+            h1.innerText="Equipa já perdeu o ovo!"
+        }
+
+        // click event listener to send
+        document.querySelector('#send').onclick = () => {
+            // get total points
+            const points = document.querySelector('points-value').value;
+
+            // get drinks from members
+            const members = document.querySelectorAll('.memberwrapper');
+            let members_values = [];
+            let puke_counter = 0, drinks_counter = 0;
+            members.forEach(member => {
+                const member_id = parseInt(member.getAttribute('data-member-id'));
+                const drinks = parseInt(member.querySelector('.dropdown').value);
+                members_values.push({
+                    "id": member_id,
+                    "points": drinks * drinkPointsValue,
+                    "drinks": drinks
+                });
+                
+                drinks_counter += drinks;
+
+                const puked = member.querySelector('.checkbox-puked').checked;
+                if (puked) puke_counter++;
+            });
+
+            // get egg
+            const egg = document.querySelector('#egg').checked;
+
+            // send data
+            fetch("http://localhost:8000/api/teamplay", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Token ' + localStorage.getItem('token')
+                },
+                body: JSON.stringify({
+                    "team_id": id,
+                    "bar_id": localStorage.getItem('bar'),
+                    "game_completed": parseInt(document.querySelector(".game > .dropdown").value) > 0,
+                    "points": points,
+                    "drinks": drinks_counter,
+                    "puked": puke_counter,
+                    "has_ee": egg,
+                    "members": members_values
+                })
+            }).then((response) => console.log(response.status));
+        }
+
+    });
+
+    // add event listener to inputs
+    document.addEventListener('input', e => {
+        const target = e.target;
+
+        // increase drink of member
+        let dropdown;
+        if ((dropdown = target.closest(".memberwrapper .dropdown"))) {
+            const n_drinks = parseInt(dropdown.value);
+            const difference = (n_drinks - Number(dropdown.getAttribute('data-drinks'))) * drinkPointsValue;
+            document.querySelector(".points-value").innerText = parseInt(document.querySelector(".points-value").innerText) + difference;
+
+            // update current n_drinks
+            dropdown.setAttribute('data-drinks', n_drinks);
+        }
+
+        // increase points from game
+        if ((dropdown = target.closest(".game .dropdown"))) {
+            const points = parseInt(dropdown.value);
+            const difference = points - Number(dropdown.getAttribute('data-points'));
+            document.querySelector(".points-value").innerText = parseInt(document.querySelector(".points-value").innerText) + difference;
+
+            // update current points
+            dropdown.setAttribute('data-points', points);
+        }
     });
 
     return(
@@ -78,8 +202,11 @@ function Equipa() {
                 <div className="section game">
                     <div className="section-title">Pontos ganhos no jogo</div>
                 </div>
+                <div className="section egg">
+                    <div className="section-title">Ovo</div>
+                </div>
 
-                <div className="button">Send</div>
+                <div className="button" id="send">Send</div>
             </div>
         </div>
     )
