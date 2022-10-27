ba65c187d67c70ec0a435c55ba035d5d1c94a3e2
Diogo Correia <diogo.correia99@ua.pt>
2022-10-27 16:10:18 +0100

    force https

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import Bares from './components/Bares';
 import Equipas from './components/Equipas';
 import Equipa from './components/Equipa';
 
+// force https
+if (window.location.protocol != "https:" && window.location.hostname != "localhost")
+  window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+
 function App() {
 
   checkSession(['/login']);
